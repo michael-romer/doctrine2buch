5501c01aa7f8dc54edb49e1e7e915e10e345d2f1
michael-romer <nl@michael-romer.de>
2016-11-11 21:45:33 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ include_once('../EntityManager.php');
 $em = new EntityManager('localhost', 'app', 'root', '');
 $user = $em->getUserRepository()->findOneById(1);
 ?>
-<p><h1><?echo $user->assembleDisplayName(); ?></h1></p>
+<p><h1><?php echo $user->assembleDisplayName(); ?></h1></p>
 <ul>
 <?php foreach($user->getPosts() as $post) { ?>
 <li><?php echo $post->getTitle(); ?></li>
